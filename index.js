3fcb4a11c0c5530b1e885caf4727c132ff80579b
holovkevych <holovkevych@gmail.com>
2023-03-06 23:06:13 +0200

    express, jwt, mongodb

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+
+import mongoose from 'mongoose'
+
+mongoose.connect('mongodb+srv://admin:testpass@cluster0.qbi2va7.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err))
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req,res) => {
+  res.send('4xd44hello world')
+})
+
+app.post('/auth/login', (req, res) => {
+  console.log(req.body)
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "John Smith",
+    },
+    'secret123'
+  )
+
+  res.json({
+    success: true,
+    token,
+  })
+})
+
+app.listen(4444, (err) => {
+  if(err) {
+    return console.log(err) 
+  }  
+
+  console.log('Server OK')
+})
