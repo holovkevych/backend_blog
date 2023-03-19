eb988dd54884edb75c5b6a1cdf94bfac49e0a636
holovkevych <holovkevych@gmail.com>
2023-03-19 20:02:55 +0200

    cors, fs

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -87,7 +87,7 @@ export const update = async (req, res) => {
       { title: req.body.title,
         text: req.body.text,
         imageUrl: req.body.imageUrl,
-        tags: req.body.tags,
+        tags: req.body.tags.split(','),
         user: req.userId, }
     )
 
@@ -102,13 +102,32 @@ export const update = async (req, res) => {
     }
 }
 
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+
+    res.json(tags);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не вдалося отримати теги',
+    });
+  }
+};
+
+
 export const create = async (req, res) => {
   try {
     const doc = new PostModel({
       title: req.body.title,
       text: req.body.text,
       imageUrl: req.body.imageUrl,
-      tags: req.body.tags,
+      tags: req.body.tags.split(','),
       user: req.userId,
     })
 
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express"
 import multer from 'multer'
+import cors from 'cors'
 import mongoose from 'mongoose'
+import fs from 'fs'
 
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js' 
 import { checkAuth, handleValidationErrors } from './utils/index.js'
 import { UserController, PostController } from './controllers/index.js'
 
-mongoose.connect('mongodb+srv://admin:admin@cluster1.y2g6dph.mongodb.net/blog?retryWrites=true&w=majority')
+mongoose
+  .connect('mongodb+srv://admin:admin@cluster1.y2g6dph.mongodb.net/blog?retryWrites=true&w=majority')
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err))
 
@@ -14,16 +17,20 @@ const app = express()
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
-    cb(null, 'uploads')
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
+    cb(null, 'uploads');
   },
   filename: (_, file, cb) => {
-    cb(null, file.originalname)
+    cb(null, file.originalname);
   },
-})
+});
 
 const upload = multer({ storage })
 
 app.use(express.json())
+app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
 app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register)
@@ -36,11 +43,21 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
   })
 })
 
-app.post('/posts', checkAuth, postCreateValidation, PostController.create)
-app.get('/posts', PostController.getAll)
-app.get('/posts/:id', PostController.getOne)
-app.delete('/posts/:id', checkAuth, PostController.remove)
-app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update)
+app.get('/tags', PostController.getLastTags);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch(
+  '/posts/:id',
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update,
+);
+
 
 app.listen(4444, (err) => {
   if (err) {
